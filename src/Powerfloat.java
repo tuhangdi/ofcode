26c2e8d76da6307bcb39e83ce75b478b6bb69908
thd-PC\thd <tu3207@126.com>
2017-02-28 12:19:58 +0800

    ·

--- src/Powerfloat.java
+++ src/Powerfloat.java
@@ -14,17 +14,35 @@ public class Powerfloat {
         }
         if(exponent < 0) abs = -exponent;
         else abs = exponent;
-        for(int i = 1; i < abs; i++){
-            result *= base;
-        }
+        //result = PowerP(base, abs);
+        result = PowerFi(base, abs);
         if(exponent < 0) result = 1 / result;
         return result;
+    }
+    public double PowerP(double base, int abs){
+        double result = 1.0;
+        for(int i = 0; i < abs; i++){
+            result *= base;
+        }
+        return  result;
+    }
+    //例如exponent = 32,则等于是base的16次方在平方，base的8次方的平方的平方。。。可以用递归做。奇数次方要多乘一个base
+    public double PowerFi(double base, int exponent){
+        if(exponent == 0) return 0;
+        if(exponent == 1) return base;
+        double result = PowerFi(base, exponent >> 1); //右移一位相当于除以2
+        result *= result;
+        if ((exponent & 1) == 1){//相当于%
+            result *= base;
+        }
+        return  result;
     }
     public static void main(String[] args){
         Powerfloat pf = new Powerfloat();
         System.out.println(pf.Power(4.5,3));
         System.out.println(pf.Power(0,-1));
         System.out.println(pf.Power(4.5,-3));
+
     }
 
 }
