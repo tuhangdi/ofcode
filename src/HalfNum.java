aa953f7a4ca325ffdfff94b3bbba37b0d5e92d36
tuhangdi <tu3207636@126.com>
2017-03-06 20:01:38 +0800

    .

---
+++ src/HalfNum.java
@@ -0,0 +1,10 @@
+/**
+ * Created by thd on 2017/3/6.
+ * 数组中有一个数字出现的次数超过数组长度的一半，请找出这个数字。
+ * 例如输入一个长度为9的数组{1,2,3,2,2,2,5,4,2}。由于数字2在数组中出现了5次，超过数组长度的一半，因此输出2。如果不存在则输出0。
+ */
+public class HalfNum {
+    public int MoreThanHalfNum_Solution(int [] array) {
+        return 0;
+    }
+}
