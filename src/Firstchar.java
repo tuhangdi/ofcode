30752912c2212231f537628a44186b244339a8fb
tuhangdi <tu3207636@126.com>
2017-03-14 16:31:02 +0800

    .

---
+++ src/Firstchar.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+
+/**
+ * Created by thd on 2017/3/10.
+ * 在一个字符串 (1<= 字符串长度 <=10000，全部由大写字母组成) 中找到第一个只出现一次的字符, 并返回它的位置
+ */
+public class Firstchar {
+    public int FirstNotRepeatingChar(String str) {
+        if (str.isEmpty() || str.length() == 0) return 0;
+        LinkedHashMap<Character, Integer> findChar = new LinkedHashMap<>();//linkedhashmap 底层是链表，可以根据顺序存储。比如说输入 c，a，hashmap 存储计算出 c，a 的 hash 值，有可能存成 a，c，而 linked 存储为 c，a 不会改变输入顺序
+        char[] strs = str.toCharArray();
+        for(int i = 0; i < str.length(); i++){
+            if(!findChar.containsKey(strs[i])) findChar.put(strs[i], 1);
+            else findChar.put(strs[i], findChar.get(strs[i]) + 1);
+        }
+        int i = 0;
+        while(i < str.length()){
+            if(findChar.get(strs[i]) == 1)
+                break;
+            i++;
+        }
+        return i;
+    }
+}
---
+++ src/UglyNumber.java
@@ -0,0 +1,56 @@
+/**
+ * Created by thd on 2017/3/10.
+ * 把只包含因子 2、3 和 5 的数称作丑数（Ugly Number）。
+ * 例如 6、8 都是丑数，但 14 不是，因为它包含因子 7。
+ * 习惯上我们把 1 当做是第一个丑数。
+ * 求按从小到大的顺序的第 N 个丑数。
+ */
+public class UglyNumber {
+    //逐个判断每个整数是不是丑数的解法，直观但不够高效
+    public int GetUglyNumber_Solution(int index) {
+        if(index <= 0) return 0;
+        int number = 0;
+        int ugly = 0;
+        while(ugly < index){
+            number ++;
+            if(isUgly(number))
+                ugly++;
+        }
+        return number;
+    }
+    private boolean isUgly(int number){
+        while(number % 2 == 0)
+            number /= 2;
+        while (number % 3 == 0)
+            number /= 3;
+        while (number % 5 == 0)
+            number /= 5;
+        return (number == 1) ? true : false;
+    }
+
+
+
+    //创建数组保存已找到的丑数，用空间换时间的解法。
+    public int GetUglyNumber(int index){
+        if(index == 0 || index == 1) return index;
+        int [] uglyArray = new int[index];
+        uglyArray[0] = 1;
+        int nextUglyNum = 1;
+        int t2 = 0, t3 = 0, t5 = 0;
+        while(nextUglyNum < index){
+            uglyArray[nextUglyNum] = Math.min(uglyArray[t2] * 2, Math.min(uglyArray[t3] * 3, uglyArray[t5] * 5));
+            while (uglyArray[t2] * 2 <= uglyArray[nextUglyNum])
+                t2++;
+            while (uglyArray[t3] * 3 <= uglyArray[nextUglyNum])
+                t3++;
+            while (uglyArray[t5] * 5 <= uglyArray[nextUglyNum])
+                t5++;
+            nextUglyNum ++;
+        }
+        return uglyArray[nextUglyNum - 1];
+    }
+    public static void main(String [] args){
+        UglyNumber un = new UglyNumber();
+        System.out.println(un.GetUglyNumber(1500));
+    }
+}
