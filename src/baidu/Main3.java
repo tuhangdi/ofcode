e27e64da2a9c2989c2a2428c1591b11e79d42ba5
thd-PC\thd <handytu@gmail.com>
2017-05-12 16:48:52 +0800

    .

--- src/baidu/Main3.java
+++ src/baidu/Main3.java
@@ -1,6 +1,10 @@
 package baidu;
 
+import java.text.DecimalFormat;
 import java.util.Scanner;
+
+import static java.lang.Math.*;
+
 
 /**
  * Created by thd on 2017/5/11.
@@ -17,12 +21,12 @@ import java.util.Scanner;
  * 输出一个数表示最大的三角形面积，保留5位小数。
  * <p>
  * 输入例子:
- * 5
- * R 0 0 0
- * R 0 4 0
- * R 0 0 3
- * G 92 14 7
- * G 12 16 8
+ 5
+ R 0 0 0
+ R 0 4 0
+ R 0 0 3
+ G 92 14 7
+ G 12 16 8
  * <p>
  * 输出例子:
  * 6.00000
@@ -31,14 +35,38 @@ public class Main3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        StringBuilder sb = new StringBuilder();
+        int[] x = new int[n];
+        int[] y = new int[n];
+        int[] z = new int[n];
+        char[] color = new char[n];
+        double maxarea = -Double.MAX_VALUE;
 
         for (int i = 0; i < n; i++) {
             String s = sc.next();
-            String[] sl = s.split(" ");
-            if (sl[0] == "R") {
-
+            char[] tep = s.toCharArray();
+            color[i] = tep[0];
+            x[i] = sc.nextInt();
+            y[i] = sc.nextInt();
+            z[i] = sc.nextInt();
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    if ((color[i] == color[j] && color[j] == color[k]) || ((color[i] != color[j]) && (color[i] != color[k]) && (color[j] != color[k]))) {
+                        double side0 = sqrt(pow(x[i] - x[j], 2) + pow(y[i] - y[j], 2) + pow(z[i] - z[j], 2));
+                        double side1 = sqrt(pow(x[i] - x[k], 2) + pow(y[i] - y[k], 2) + pow(z[i] - z[k], 2));
+                        double side2 = sqrt(pow(x[k] - x[j], 2) + pow(y[k] - y[j], 2) + pow(z[k] - z[j], 2));
+                        if (side0 + side1 <= side2 || side0 + side2 <= side1 || side1 + side2 <= side0) {
+                            continue;
+                        }
+                        double p = (side0 + side1 + side2) / 2;
+                        double area = sqrt(p * (p - side0) * (p - side1) * (p - side2));
+                        if (area > maxarea) maxarea = area;
+                    }
+                }
             }
         }
+        DecimalFormat df = new DecimalFormat("###.00000");
+        System.out.println(df.format(maxarea));
     }
 }
