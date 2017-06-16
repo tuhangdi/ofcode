6a58aa7e6c892cac0ccf50e94ff10fd411ae80be
handytu@gmail.com <handytu@gmail.com>
2017-06-16 21:12:45 +0800

    .

--- src/nowcoder616/Main2.java
+++ src/nowcoder616/Main2.java
@@ -9,17 +9,18 @@ import java.util.Scanner;
 public class Main2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-
-        if (n == 0 || n == 1) System.out.println(n + 1);
-        else {
-            int [] num = new int[n];
-            num[0] = 2;
-            for (int i = 1; i < n; i++) {
-                num[i] = num[i - 1] * 2 + i;
+        while (sc.hasNext()) {
+            int n = sc.nextInt();
+            if (n == 0 || n == 1) System.out.println(n + 1);
+            else {
+                int[] num = new int[n];
+                num[0] = 2;
+                for (int i = 1; i < n; i++) {
+                    num[i] = num[i - 1] * 2 + i;
+                }
+                double f = (double) num[n - 1] / Math.pow(2, n - 1);
+                System.out.print(String.format("%.1f", f));
             }
-            double f = (double)num[n -1] /Math.pow(2,n - 1);
-            System.out.print(String.format("%.1f", f));
         }
     }
 }
