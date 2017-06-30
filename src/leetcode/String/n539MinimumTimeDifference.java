61ffcbb1b0ecc7722c07d28cb971eb32c840c43e
handytu@gmail.com <handytu@gmail.com>
2017-06-30 16:33:30 +0800

    Merge branch 'master' of https://github.com/tuhangdi/ofcode
    # Conflicts:
    #	.idea/workspace.xml

--- src/leetcode/String/n539MinimumTimeDifference.java
+++ src/leetcode/String/n539MinimumTimeDifference.java
@@ -1,5 +1,10 @@
 package leetcode.String;
 
+import org.omg.PortableInterceptor.INACTIVE;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -13,6 +18,32 @@ import java.util.List;
  */
 public class n539MinimumTimeDifference {
     public int findMinDifference(List<String> timePoints) {
+        List<Integer> time = new ArrayList<>();
+        for (int i = 0; i < timePoints.size(); i++) {
+            String[] str = timePoints.get(i).split(":");
+            int h = Integer.valueOf(str[0]);
+            int m = Integer.valueOf(str[1]);
+            time.add(h * 60 + m);
+        }
+        Collections.sort(time);
+        int min = Integer.MAX_VALUE;
+        for (int i = 1; i < timePoints.size(); i++) {
+            int tim = Math.min(Math.abs(time.get(i) - time.get(i-1)), 1440 - Math.abs(time.get(i) - time.get(i-1)));
+            if (tim < min)
+                min = tim;
+        }
+        int tim = Math.min(Math.abs(time.get(0) - time.get(timePoints.size()-1)), 1440 - Math.abs(time.get(0) - time.get(timePoints.size()-1)));
+        if (tim < min)
+            min = tim;
 
+        return min;
+    }
+
+    public static void main(String[] args) {
+        List<String> timePoints = new ArrayList<>();
+        timePoints.add("23:59");
+        timePoints.add("00:00");
+        n539MinimumTimeDifference n = new n539MinimumTimeDifference();
+        System.out.println(n.findMinDifference(timePoints));
     }
 }
