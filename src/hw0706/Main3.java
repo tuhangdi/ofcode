d2f7741658393c9ccfd690cd52c625cf68926783
tuhangdi <tu3207636@126.com>
2017-07-06 20:06:49 +0800

    .

---
+++ src/hw0706/Main3.java
@@ -0,0 +1,19 @@
+package hw0706;
+
+import java.util.Scanner;
+
+/**
+ * Created by thd on 2017/7/6.
+ */
+public class Main3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            String f = sc.next();
+            String s = sc.next();
+
+        }
+    }
+
+}
