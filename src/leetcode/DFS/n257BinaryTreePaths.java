0561fe336c4d04c3658ba4481c82cf8d0d7211b1
handytu@gmail.com <handytu@gmail.com>
2017-07-17 22:05:16 +0800

    .

--- src/leetcode/DFS/n257BinaryTreePaths.java
+++ src/leetcode/DFS/n257BinaryTreePaths.java
@@ -39,4 +39,15 @@ public class n257BinaryTreePaths {
         }
         sb.setLength(len);
     }
+    public static void main(String[] args) {
+        TreeNode t1 = new TreeNode(1);
+        TreeNode t2 = new TreeNode(2);
+        TreeNode t3 = new TreeNode(3);
+        TreeNode t4 = new TreeNode(4);
+        t1.left = t2;
+        t1.right = t3;
+        t2.left = t4;
+        n257BinaryTreePaths n = new n257BinaryTreePaths();
+        System.out.println(n.binaryTreePaths(t1).toString());
+    }
 }
